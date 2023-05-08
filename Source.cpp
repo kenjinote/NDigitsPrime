459c6629e847680b6554c2486b44d52616acfbd5
kenjinote <2605401+kenjinote@users.noreply.github.com>
2023-05-08 21:19:52 +0900

    プロジェクト ファイルを追加します。

---
+++ Source.cpp
@@ -0,0 +1,92 @@
+﻿#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include <windows.h>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/multiprecision/miller_rabin.hpp>
+#include <boost/multiprecision/random.hpp>
+
+using namespace boost::random;
+using namespace boost::multiprecision;
+
+TCHAR szClassName[] = TEXT("Window");
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	static HWND hEdit1;
+	static HWND hEdit2;
+	static HWND hButton;
+	switch (msg)
+	{
+	case WM_CREATE:
+		hEdit1 = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), 0, WS_VISIBLE | WS_CHILD | ES_AUTOHSCROLL | ES_NUMBER, 0, 0, 0, 0, hWnd, (HMENU)1000, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		hButton = CreateWindow(TEXT("BUTTON"), TEXT("探索"), WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, hWnd, (HMENU)IDOK, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		hEdit2 = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), 0, WS_VISIBLE | WS_CHILD | ES_AUTOHSCROLL | ES_READONLY, 0, 0, 0, 0, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		SendMessage(hWnd, WM_DPICHANGED, 0, 0);
+		break;
+	case WM_SIZE:
+		MoveWindow(hEdit1, 10, 10, 256, 32, TRUE);
+		MoveWindow(hButton, 10, 50, 256, 32, TRUE);
+		MoveWindow(hEdit2, 10, 90, 256, 32, TRUE);
+		break;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK)
+		{
+			SetWindowText(hEdit2, 0);
+			boost::random::mt19937 gen;
+			int n = GetDlgItemInt(hWnd, 1000, 0, 0);
+			for (cpp_int m = pow(cpp_int(10),n) - 3; m > 0; m -= 2) {
+				if (miller_rabin_test(m, 25, gen)) {
+					std::string str = m.str();
+					SetWindowTextA(hEdit2, str.c_str());
+					break;
+				}
+			}
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPWSTR pCmdLine, int nCmdShow)
+{
+	MSG msg;
+	WNDCLASS wndclass = {
+		CS_HREDRAW | CS_VREDRAW,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		0,
+		LoadCursor(0,IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1),
+		0,
+		szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd = CreateWindow(
+		szClassName,
+		TEXT("指定桁の最大の素数を探索する"),
+		WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		0,
+		0,
+		hInstance,
+		0
+	);
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
